ef48abe05a52599c9801ed7d94819ce2a7d9d47d
Angélika Andreatta Barros Corrêa <angelika.andreatta@dtidigital.com.br>
2022-09-11 16:44:57 -0300

    Mapping and DbContext creation

---
+++ Api/Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
