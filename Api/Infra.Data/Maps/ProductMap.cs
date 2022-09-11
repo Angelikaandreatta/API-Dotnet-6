45310397c9b0ee13dd97e761a4e39dddcb381ffd
Angélika Andreatta Barros Corrêa <angelika.andreatta@dtidigital.com.br>
2022-09-11 17:14:12 -0300

    MapProduct creation

---
+++ Api/Infra.Data/Maps/ProductMap.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Data.Maps
+{
+    public class ProductMap : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Product");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasColumnName("Id_Product")
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .HasColumnName("Name_Product");
+
+            builder.Property(x => x.CodErp)
+                .HasColumnName("CodErp_Product");
+
+            builder.Property(x => x.Price)
+                .HasColumnName("Price_Product");
+
+            builder.HasMany(x => x.Purchase)
+                .WithOne(x => x.Product)
+                .HasForeignKey(x => x.ProductId);
+        }
+    }
+}
