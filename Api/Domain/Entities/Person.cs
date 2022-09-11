10d16abbb3821915732189b93ded51bed1f3822c
Angélika Andreatta Barros Corrêa <angelika.andreatta@dtidigital.com.br>
2022-09-11 15:26:42 -0300

    API creation, person entity and validations

---
+++ Api/Domain/Entities/Person.cs
@@ -0,0 +1,31 @@
+﻿namespace Domain.Entities
+{
+    public sealed class Person
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Document { get; private set; }
+        public string Phone { get; private set; }
+
+        public Person(string name, string document, string phone)
+        {
+            Validation(name, document, phone);
+        }
+
+        public Person(int id, string name, string document, string phone)
+        {
+            Domain.Validations.DomainValidationException.When(id <= 0, "Id must be greater than zero");
+            Id = id;
+            Validation(name, document, phone);
+        }
+
+        private void Validation(string name, string document, string phone)
+        {
+            Domain.Validations.DomainValidationException.When(string.IsNullOrEmpty(name), "Name must be informed.");
+            Domain.Validations.DomainValidationException.When(string.IsNullOrEmpty(document), "Document must be informed.");
+            Domain.Validations.DomainValidationException.When(string.IsNullOrEmpty(phone), "Phone must be informed.");
+
+            (Name, Document, Phone) = (name, document, phone);
+        }
+    }
+}
---
+++ Api/Domain/Validations/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Validations
+{
+    public class DomainValidationException : Exception
+    {
+        public DomainValidationException(string error) : base(error)
+        { }
+        public static void When(bool hasError, string message)
+        {
+            if (hasError)
+                throw new DomainValidationException(message);
+        }
+    }
+}
