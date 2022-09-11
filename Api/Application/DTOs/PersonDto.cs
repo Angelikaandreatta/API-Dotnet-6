6bd2cc6b1fa289cf92bd38e330e5657eee4dc704
Angélika Andreatta Barros Corrêa <angelika.andreatta@dtidigital.com.br>
2022-09-11 18:46:47 -0300

    Creating DtoPerson and validations

---
+++ Api/Application/DTOs/PersonDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Api/Application/DTOs/Validations/PersonDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace Application.DTOs.Validations
+{
+    public class PersonDtoValidator : AbstractValidator<PersonDto>
+    {
+
+        public PersonDtoValidator()
+        {
+            RuleFor(x => x.Name)
+               .NotEmpty()
+               .NotNull()
+               .WithMessage("Inform your name");
+
+            RuleFor(x => x.Document)
+               .NotEmpty()
+               .NotNull()
+               .WithMessage("Inform your document");
+
+            RuleFor(x => x.Phone)
+              .NotEmpty()
+              .NotNull()
+              .WithMessage("Inform your phone");
+        }
+    }
+}
