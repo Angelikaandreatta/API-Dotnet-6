0dbae80217f9412fc39b034651eab16a792c51a2
Angélika Andreatta Barros Corrêa <angelika.andreatta@dtidigital.com.br>
2022-09-11 17:22:03 -0300

    MapPurchase creation

---
+++ Api/Infra.Data/Maps/PurchaseMap.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Data.Maps
+{
+    public class PurchaseMap : IEntityTypeConfiguration<Purchase>
+    {
+        public void Configure(EntityTypeBuilder<Purchase> builder)
+        {
+            builder.ToTable("Purchase");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasColumnName("Id_Purchase")
+                .UseIdentityColumn();
+
+            builder.Property(x => x.PersonId)
+               .HasColumnName("Id_Person");
+
+            builder.Property(x => x.ProductId)
+               .HasColumnName("Id_Product");
+
+            builder.Property(x => x.Date)
+               .HasColumnName("Date_Purchase");
+
+            builder.HasOne(x => x.Person)
+                .WithMany(x => x.Purchase);
+
+            builder.HasOne(x => x.Product)
+            .WithMany(x => x.Purchase);
+        }
+    }
+}
