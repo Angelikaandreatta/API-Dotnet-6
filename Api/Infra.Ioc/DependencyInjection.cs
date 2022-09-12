bf3190fd5a1402ba5da384a90f7dc691e24c495a
Angélika Andreatta Barros Corrêa <angelika.andreatta@dtidigital.com.br>
2022-09-11 21:27:13 -0300

    Dependency injection creation

---
+++ Api/Infra.Ioc/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Application.Mappings;
+using Application.Services;
+using Application.Services.Interfaces;
+using Domain.Repositories;
+using Infra.Data.Context;
+using Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.Ioc
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContextFactory<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("")));
+            services.AddScoped<IPersonRepository, PersonRepository>();
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(typeof(DomainToDtoMapping));
+            services.AddScoped<IPersonService, PersonService>();
+            return services;
+        }
+    }
+}
